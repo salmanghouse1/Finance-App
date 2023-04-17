f31f9d0ca5e0cee894f9e440e1d7394166bf202f
salman ghouse <salmanghouse1@gmail.com>
2023-04-16 20:48:17 -0400

    significantly improved front end for stock list

--- public/assets/js/stock-list.js
+++ public/assets/js/stock-list.js
@@ -20,20 +20,20 @@ const getStocksList = () => {
 
 const printStock = ({ _id, stockName, createdBy, createdAt,currentPrice,soldPrice,boughtPrice,commentCount}) => {
   const stocksCard = `
-    <div class="col-12 col-lg-6 flex-row">
+    <div class="col-12 col-lg-6 flex-row box" style="border:20px solid black;border-radius:7px;">
       <div class="card w-100 flex-column">
         <h3 class="card-header">${stockName}</h3>
         <div class="card-body flex-column col-auto">
-          <h4 class="text-dark">By ${createdBy}</h4>
-          <p>On ${createdAt}</p>
+          <h4 class="text-dark">By <b>${createdBy}</b></h4>
+          <p>${createdAt}</p>
           
-          <p>${commentCount} Comments</p>
-          <p>${currentPrice}</p>
-          <p>${soldPrice}</p>
-          <p>${boughtPrice}</p>  
+          <p><b>${commentCount}</b> Comments</p>
+          <p><b>currentPrice:</b>${currentPrice}</p>
+          <p><b>Sold Price:</b>${soldPrice}</p>
+          <p><b>Bought Price:</b>${boughtPrice}</p>  
           <ul>
           </ul>
-          <a class="btn display-block w-100 mt-auto" href="/stock?id=${_id}">See the discussion.</a>
+          <a class="btn display-block w-100 mt-auto" href="/stock?id=${_id}" style="font-weight:bold;text-decoration:underline">See the discussion.</a>
         </div>
       </div>
     </div>
