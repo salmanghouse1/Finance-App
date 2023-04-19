12d90046c4c46fbe57aa2ef23734056501bcf5f1
salman ghouse <salmanghouse1@gmail.com>
2023-04-18 20:43:15 -0400

    deploying

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 const express = require('express');
-const Stock = require('./models/index');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -15,30 +15,5 @@ app.use(require('./routes'));
 
 mongoose.connect(process.env.MONGO_DB_URI, {useNewUrlParser:true,useUnifiedTopology:true});
 
-// const seedStocks =[
-//         {
-//         "stockName": "Bitcoin",
-//         "createdBy": "Salman",
-//            "currentPrice":"5",
-//            "soldPrice":"6",
-//            "boughtPrice":"3",
-//            "comments":[{
-//                "writtenBy":"Salman",
-//                "commentBody":"Great"
-//            }]
-        
-//        }
-//     ]
-
-// const seedDB = async () => {
-//     await Stock.deleteMany({});
-//     await Stock.insertMany(seedStocks);
-// };
-
-// seedDB().then(() => {
-//     mongoose.connection.close();
-// })
-
-// mongoose.set(debug,true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
