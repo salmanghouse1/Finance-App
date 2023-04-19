72026833c3de91d5bf4befc8c5b382d612039474
salman ghouse <salmanghouse1@gmail.com>
2023-04-18 20:17:56 -0400

    deploying

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.STARTING_VAR+encodeURIComponent(process.env.MONGO_DB_URI), {useNewUrlParser:true,useUnifiedTopology:true});
+mongoose.connect(process.env.MONGO_DB_URI, {useNewUrlParser:true,useUnifiedTopology:true});
 
 // const seedStocks =[
 //         {
