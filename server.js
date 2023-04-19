aac65c7be32a27aaafc5ff04459d6038fa35a7e8
salman ghouse <salmanghouse1@gmail.com>
2023-04-18 20:35:50 -0400

    deploying

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+
 app.use(require('./routes'));
 
 mongoose.connect(process.env.MONGO_DB_URI, {useNewUrlParser:true,useUnifiedTopology:true});
